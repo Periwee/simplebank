9bebee216b369c6f96ccfa3dfc69a4f3ce80b213
Esperanza Rodriguez <periwee@Esperanzas-MacBook-Pro.local>
2026-02-23 19:38:37 -0800

    fix import

--- api/server.go
+++ api/server.go
@@ -2,7 +2,7 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	db "github.com/techschool/simplebank/db/sqlc"
+	db "github.com/periwee/simplebank/db/sqlc"
 )
 
 // Server serves HTTP requests for our banking service.
